a0eab6298bb33b79260ec9ed9a14c6b412717ac2
Cipactli Casillas <c.casillas19@gmail.com>
2025-05-20 23:43:06 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,6 +1,6 @@
-const s=document.querySelector(".js_searcher"),i=document.querySelector(".js_btn_search"),l=document.querySelector(".js_btn_reset");l.addEventListener("click",e=>{e.preventDefault(),s.value=""});i.addEventListener("click",o);const r=document.querySelector(".js_anime_listUl");let a=[];function m(e){return`
-<li class="js_anime_Item">
+const i=document.querySelector(".js_searcher"),m=document.querySelector(".js_btn_search"),d=document.querySelector(".js_btn_reset"),u=document.querySelector(".js_anime_listUl"),h=document.querySelector(".js_favouritesUl");d.addEventListener("click",e=>{e.preventDefault(),i.value=""});m.addEventListener("click",_);let c=[];function a(e){return`
+<li class="js_animeLi" data-gancho="${e.mal_id}">
   <h2 class="js_title">${e.title}</h2>
   <img class="js_image" src="${e.images.jpg.image_url}" alt="imagen de anime" />
-</li>`}function c(){let e="";for(const t of a)e+=m(t);r.innerHTML=e}fetch("https://api.jikan.moe/v4/anime").then(e=>e.json()).then(e=>{a=e.data,c()});function o(e){e.preventDefault();const t=s.value;fetch(`https://api.jikan.moe/v4/anime?q=${t}`).then(n=>n.json()).then(n=>{a=n.data,c()})}
+</li>`}function s(){let e="";for(const t of c)e+=a(t);u.innerHTML=e;const n=document.querySelectorAll(".js_animeLi");for(const t of n)t.addEventListener("click",f)}function f(e){const n=e.currentTarget;n.classList.toggle("favourite");const t=n.dataset.gancho,l=c.find(r=>r.mal_id===parseInt(t)),o=a(l);h.innerHTML+=o}fetch("https://api.jikan.moe/v4/anime").then(e=>e.json()).then(e=>{c=e.data,s()});function _(e){e.preventDefault();const n=i.value;fetch(`https://api.jikan.moe/v4/anime?q=${n}`).then(t=>t.json()).then(t=>{c=t.data,s()})}
 //# sourceMappingURL=main.js.map
