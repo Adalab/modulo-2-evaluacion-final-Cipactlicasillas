ec9e9c50cf2351cc93ff11efe6026ecbcab40adf
Cipactli Casillas <c.casillas19@gmail.com>
2025-05-20 23:43:28 +0200

    cambios favoritos

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@
 const inputSearch = document.querySelector(".js_searcher");
 const btnSearch = document.querySelector(".js_btn_search");
 const btnReset = document.querySelector(".js_btn_reset");
+const animeListUl = document.querySelector(".js_anime_listUl");
+const favouritesUl = document.querySelector(".js_favouritesUl");
 
 btnReset.addEventListener("click", (event) => {
   event.preventDefault();
@@ -10,13 +12,11 @@ btnReset.addEventListener("click", (event) => {
 });
 btnSearch.addEventListener("click", handleClickBtnSearch);
 
-const animeListUl = document.querySelector(".js_anime_listUl");
-
 let allAnimes = [];
 
 function renderOneAnime(oneAnime) {
   const html = `
-<li class="js_anime_Item">
+<li class="js_animeLi" data-gancho="${oneAnime.mal_id}">
   <h2 class="js_title">${oneAnime.title}</h2>
   <img class="js_image" src="${oneAnime.images.jpg.image_url}" alt="imagen de anime" />
 </li>`;
@@ -28,8 +28,23 @@ function renderallAnimes() {
   for (const oneAnime of allAnimes) {
     html += renderOneAnime(oneAnime);
   }
-
   animeListUl.innerHTML = html;
+  const allAnimeLi = document.querySelectorAll(".js_animeLi");
+
+  for (const animeLi of allAnimeLi) {
+    animeLi.addEventListener("click", handleClickAnime);
+  }
+}
+
+function handleClickAnime(ev) {
+  const clickedLi = ev.currentTarget;
+  clickedLi.classList.toggle("favourite");
+  const id_gancho = clickedLi.dataset.gancho;
+  const clickedAnimeObject = allAnimes.find(
+    (oneAnime) => oneAnime.mal_id === parseInt(id_gancho)
+  );
+  const htmloneAnime = renderOneAnime(clickedAnimeObject);
+  favouritesUl.innerHTML += htmloneAnime;
 }
 
 fetch("https://api.jikan.moe/v4/anime")
