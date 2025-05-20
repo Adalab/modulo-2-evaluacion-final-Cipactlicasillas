bdf63fc8a76edbc2ac995862280b6843721ddf32
Cipactli Casillas <c.casillas19@gmail.com>
2025-05-20 09:56:11 +0200

    botones+eventos

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const searcherElement = document.querySelector(".js_Searcher");
+const btnSearch = document.querySelector(".js_BtnSearch");
+const btnReset = document.querySelector(".js_BtnReset");
+
+searcherElement.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+btnSearch.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+btnReset.addEventListener("click", (event) => {
+  event.preventDefault();
+  input.value = "";
+});
